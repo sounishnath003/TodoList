02328c079d0f22e680a74c4d7405aaab1073c9e4
Sounish Nath <flock.sinasini@gmail.com>
2018-08-14 18:37:21 +0530

    Availbiliaty to Mark_as Completed!!_nicely Done With Great Job

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
-    before_action :set_todo_list
+    before_action :set_todo_list   # checking and Assuring At First
+    before_action :set_todo_item, except: [:create]  # checking and Assuring At First 
 def create
    @todo_item = @todo_list.todo_items.create(set_todo_item_params)
    redirect_to  @todo_list
@@ -15,10 +16,19 @@ if @todo_item.destroy
     redirect_to @todo_list
 end
 
+def  complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "To-do Item Is Completed." 
+end
+
 private
     # Setting the Env for Todo_list
     def set_todo_list
    @todo_list = TodoList.find(params[:todo_list_id])  
+    end
+    # Destroy_config test For Multiple Usuage
+    def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
     end
     # Setting For teh Todo_items in Todo_ist.included
     def set_todo_item_params
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,7 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+    # Below Code Cecks The To-Do Item if completed?"yes":"not "
+  def completed?
+      !completed_at.blank? # This Means The Completed_at field Will not Blank, its Contains Time.now(:val)
+  end
 end
